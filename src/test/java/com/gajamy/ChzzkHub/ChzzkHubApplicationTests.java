cece9ac56e6f1b17dcc090a0bc0f6952e9e02332
GaJaMy <mansa0805@gmail.com>
2025-09-14 19:19:14 +0900

    chore : README 작성

---
+++ src/test/java/com/gajamy/ChzzkHub/ChzzkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gajamy.ChzzkHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChzzkHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
